804b3d61e042a25ba3cdfd4bddf57faca48cbe4a
christophe seguinot <Christophe.SEGUINOT@sustainecho.com>
2025-05-21 11:14:18 +0200

    Refactor NavBar to use string literals for navigation paths, simplifying the code and improving readability.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,18 @@
 import styles from './NavBar.module.scss';
 import { useTranslation } from 'react-i18next';
 import { CustomLink } from '@components/CustomLink';
-import { PATHS } from '@src/router';
 
 export const NavBar = () => {
   const { t } = useTranslation();
   return (
     <div className={`${styles['root']}`}>
-      <CustomLink pathType="static" path={PATHS.HOME as keyof typeof PATHS} type="nav">
+      <CustomLink pathType="static" path="HOME" type="nav">
         {t('common.page.search.link')}
       </CustomLink>
-      <CustomLink pathType="static" path={PATHS.NEW as keyof typeof PATHS} type="nav">
+      <CustomLink pathType="static" path="NEW" type="nav">
         {t('common.page.create.link')}
       </CustomLink>
-      <CustomLink pathType="static" path={PATHS.VALIDATION as keyof typeof PATHS} type="nav">
+      <CustomLink pathType="static" path="VALIDATION" type="nav">
         {t('common.page.validation.link')}
       </CustomLink>
     </div>
