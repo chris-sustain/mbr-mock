2516edf0eede84cee6b99e87e4b61fff57f1c934
christophe seguinot <Christophe.SEGUINOT@sustainecho.com>
2025-05-21 10:50:07 +0200

    Update NavBar to use PATHS constants for navigation links, enhancing route management and improving code clarity.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,19 @@
 import styles from './NavBar.module.scss';
 import { useTranslation } from 'react-i18next';
 import { CustomLink } from '@components/CustomLink';
+import { PATHS } from '@src/router';
 
 export const NavBar = () => {
   const { t } = useTranslation();
   return (
     <div className={`${styles['root']}`}>
-      <CustomLink path="HOME" type="nav">
+      <CustomLink pathType="static" path={PATHS.HOME as keyof typeof PATHS} type="nav">
         {t('common.page.search.link')}
       </CustomLink>
-      <CustomLink path="NEW" type="nav">
+      <CustomLink pathType="static" path={PATHS.NEW as keyof typeof PATHS} type="nav">
         {t('common.page.create.link')}
       </CustomLink>
-      <CustomLink path="VALIDATION" type="nav">
+      <CustomLink pathType="static" path={PATHS.VALIDATION as keyof typeof PATHS} type="nav">
         {t('common.page.validation.link')}
       </CustomLink>
     </div>
