e842d10156a133bde8915952689d8e6c0da58420
christophe seguinot <Christophe.SEGUINOT@sustainecho.com>
2025-05-21 11:25:22 +0200

    Refactor CreateRefFromDraftPage, CreateRefPage, and EditRefPage components to use direct className assignment, improving code consistency and readability.

--- src/components/CreateRefFromDraftPage/CreateRefFromDraftPage.tsx
+++ src/components/CreateRefFromDraftPage/CreateRefFromDraftPage.tsx
@@ -1,5 +1,5 @@
 import styles from './CreateRefFromDraftPage.module.scss';
 
 export const CreateRefFromDraftPage = () => {
-  return <div className={`${styles['root']}`}>{'CreateRefFromDraftPage'}</div>;
+  return <div className={styles['root']}>{'CreateRefFromDraftPage'}</div>;
 };
--- src/components/EditRefPage/EditRefPage.tsx
+++ src/components/EditRefPage/EditRefPage.tsx
@@ -1,5 +1,5 @@
 import styles from './EditRefPage.module.scss';
 
 export const EditRefPage = () => {
-  return <div className={`${styles['root']}`}>{'EditRefPage'}</div>;
+  return <div className={styles['root']}>{'EditRefPage'}</div>;
 };
